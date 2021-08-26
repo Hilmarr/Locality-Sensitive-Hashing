750f34986bdfc8ca05da568f053675bc3b26fafa
Hilmar <hilmarwiderberg@live.no>
2021-08-26 21:08:34 +0200

    Changed name of a variable + some cleanup

--- LSH_attempt_1.m
+++ LSH_attempt_1.m
@@ -24,15 +24,15 @@ naiveMatchingStart = tic;
 matching_naive = zeros(nPoints, 1);
 
 for i = 1:nPoints
-    bestMatch = 1e10;
+    shortestDist = 1e10;
     match = -1;
     for j = 1:nPoints
         % Take euclidean distance between the vector
         diff = sum((points1(i,:) - points2(j,:)) .^ 2);
         % If euclidean distance is better than the best match,
         % we have a new best match
-        if (diff < bestMatch)
-            bestMatch = diff;
+        if (diff < shortestDist)
+            shortestDist = diff;
             match = j;
         end
     end
@@ -49,7 +49,7 @@ hyperplanes = 2*rand(nPlanes, vectorLength) - 1;
 maxHPLength = 1;
 
 % % Give the hyperplanes the length of somewhere between 0 and maxHPLength
-% % Not needed if asll hyperplanes go through the origin
+% % Not needed if all hyperplanes go through the origin
 % for i = 1:nPlanes
 %     hyperplanes(i,:) = hyperplanes(i,:) ...
 %         * (maxHPLength * rand(1) / norm(hyperplanes(i,:), 2));
@@ -86,16 +86,6 @@ for i = 1:nPoints
     groupSizeMap(hashcode) = groupSizeMap(hashcode)+1;
 end
 hashValuesTime1 = toc(hashValuesStart1);
-
-% % LATER: count duplicates in indexGroupMap
-% % i.e. the number of points that gets mapped to a box where there already
-% %      exists some points
-% % (in order to check whether using hyperplanes that go through the origin
-% %  is better than more random hyperplanes, should probably plot number
-% %  of duplicates given the length of the hyperplanes)
-% nDuplicates = sum(groupSizeMap(groupSizeMap > 1)-1);
-% % See the maximum amount of points that get mapped to the same box
-% largestBox = max(groupSizeMap);
 
 % Prepare the index map
 cnt = 1;
@@ -155,7 +145,7 @@ matching_lsh = zeros(nPoints, 1);
 
 for i = 1:nPoints
     % Find table and elements in points1 to match with
-    bestMatch = 1e10;
+    shortestDist = 1e10;
     hashcode = indexGroupMap(i);
     size = groupSizeMap(hashcode);
     startIdx = groupIndexMap(hashcode);
@@ -167,12 +157,12 @@ for i = 1:nPoints
         diff = sum((points2(i,:) - points1(idx,:)) .^ 2);
         % If euclidean distance is better than the best match,
         % we have a new best match
-        if (diff < bestMatch)
-            bestMatch = diff;
+        if (diff < shortestDist)
+            shortestDist = diff;
             match = idx;
         end
     end
-    if (bestMatch ~= 1e10)
+    if (shortestDist ~= 1e10)
         matching_lsh(i) =  match;
     else
         matching_lsh(i) = -1;
