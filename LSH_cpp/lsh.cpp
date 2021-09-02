96aa66444e29166a08a954cbf2136118ff750bb7
Hilmar <hilmarwiderberg@live.no>
2021-09-02 14:15:26 +0200

    Changed structure of the program, now putting all variables inside the main function

--- LSH_cpp/lsh.cpp
+++ LSH_cpp/lsh.cpp
@@ -1,46 +1,52 @@
 #include <iostream>
 #include <cmath>
 
-const int nPoints = 1000;
-const int vectorLength = 128;
-const float noiseScale = 0.1;
-
-// points to be matched
-double points1[nPoints][vectorLength];
-double points2[nPoints][vectorLength];
-
 inline double rand_minus1_to_1() {
     return ((double)rand() / (RAND_MAX/2)) - 1;
 }
 
-void fill_point_arrays();
+void fill_point_arrays(int nPoints, int vectorLength, int noiseScale,
+                       double* points1, double* points2);
 
 int main() {
-    
-    fill_point_arrays();
+    const int nPoints = 1000;
+    const int vectorLength = 128;
+    const float noiseScale = 0.1;
+
+    // points to be matched
+    double points1[nPoints * vectorLength];
+    double points2[nPoints * vectorLength];
+
+    const int nPlanes = (int)log2(nPoints);
+
+    fill_point_arrays(nPoints, vectorLength, noiseScale, points1, points2);
 
     double size = 0;
     for (int j = 0; j < vectorLength; j++) {
-        size += points1[0][j] * points1[0][j];
-        printf("%f\n", points1[0][j]);
+        size += points1[j] * points1[j];
+        printf("%f\n", points1[j]);
     }
     printf("\nsize: %f\n", sqrt(size));
+
+    // double hyperplanes[nPlanes][vectorLength];
 }
 
-void fill_point_arrays() {
+void fill_point_arrays(int nPoints, int vectorLength, int noiseScale,
+                       double* points1, double* points2)
+{
     for (int i = 0; i < nPoints; i++) {
         double size1 = 0;
         double size2 = 0;
         for (int j = 0; j < vectorLength; j++) {
-            // points1[i] = random array with values between -1 and 1
+            // points1[i][:] = random array with values between -1 and 1
             double rn = rand_minus1_to_1();
-            points1[i][j] = rn;
-            // points2[i] = points1[i] + noise
+            points1[i*vectorLength + j] = rn;
+            // points2[i][:] = points1[i][:] + noise
             double noise = noiseScale * rand_minus1_to_1();
-            points2[i][j] = rn + noise;
+            points2[i*vectorLength + j] = rn + noise;
             // Keep track of euclidean lengths to normalize after loop
-            size1 += points1[i][j] * points1[i][j];
-            size2 += points2[i][j] * points2[i][j];
+            size1 += points1[i*vectorLength + j] * points1[i*vectorLength + j];
+            size2 += points2[i*vectorLength + j] * points2[i*vectorLength + j];
         }
 
         size1 = sqrt(size1);
@@ -48,8 +54,8 @@ void fill_point_arrays() {
 
         // normalize the vectors
         for (int j = 0; j < vectorLength; j++) {
-            points1[i][j] /= size1;
-            points2[i][j] /= size2;
+            points1[i*vectorLength + j] /= size1;
+            points2[i*vectorLength + j] /= size2;
         }
     }
 }
