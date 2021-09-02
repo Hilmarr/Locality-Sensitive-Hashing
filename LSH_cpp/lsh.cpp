4f1591980e2eab609188734998e57d6e16104f68
Hilmar <hilmarwiderberg@live.no>
2021-09-02 16:42:00 +0200

    Creating normalized hyperplanes

--- LSH_cpp/lsh.cpp
+++ LSH_cpp/lsh.cpp
@@ -8,6 +8,8 @@ inline double rand_minus1_to_1() {
 void fill_point_arrays(int nPoints, int vectorLength, int noiseScale,
                        double* points1, double* points2);
 
+void fill_hyperplanes(int nPlanes, int vectorLength, double* hyperplanes);
+
 int main() {
     const int nPoints = 1000;
     const int vectorLength = 128;
@@ -21,14 +23,11 @@ int main() {
 
     fill_point_arrays(nPoints, vectorLength, noiseScale, points1, points2);
 
-    double size = 0;
-    for (int j = 0; j < vectorLength; j++) {
-        size += points1[j] * points1[j];
-        printf("%f\n", points1[j]);
-    }
-    printf("\nsize: %f\n", sqrt(size));
-
-    // double hyperplanes[nPlanes][vectorLength];
+    // allocate hyperplanes
+    double hyperplanes[nPlanes * vectorLength];
+    // create normalized hyperplanes represented by vectors of euclidean size 1
+    fill_hyperplanes(nPlanes, vectorLength, hyperplanes);
+    
 }
 
 void fill_point_arrays(int nPoints, int vectorLength, int noiseScale,
@@ -56,6 +55,27 @@ void fill_point_arrays(int nPoints, int vectorLength, int noiseScale,
         for (int j = 0; j < vectorLength; j++) {
             points1[i*vectorLength + j] /= size1;
             points2[i*vectorLength + j] /= size2;
+        }
+    }
+}
+
+void fill_hyperplanes(int nPlanes, int vectorLength, double* hyperplanes) {
+
+    for (int i = 0; i < nPlanes; i++) {
+        double size = 0;
+        for (int j = 0; j < vectorLength; j++) {
+            // hyperplanes[i][:] = random array with values between -1 and 1
+            hyperplanes[i*vectorLength + j] = rand_minus1_to_1();
+            // Keep track of euclidean lengths to normalize after loop
+            size += hyperplanes[i * vectorLength + j] * hyperplanes[i * vectorLength + j];
+        }
+
+        
+        size = sqrt(size);
+        double invsize = 1/size;
+        // normalize the vectors
+        for (int j = 0; j < vectorLength; j++) {
+            hyperplanes[i * vectorLength + j] *= invsize;
         }
     }
 }
