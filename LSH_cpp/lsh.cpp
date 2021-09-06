aacda6af1a6d43e1fc53185112f9c91e418b5b6b
Hilmar <hilmarwiderberg@live.no>
2021-09-06 18:26:17 +0200

    Now able to match points, also fixed some massive bugs

--- LSH_cpp/lsh.cpp
+++ LSH_cpp/lsh.cpp
@@ -7,21 +7,22 @@ inline double rand_minus1_to_1() {
     return ((double)rand() / (RAND_MAX/2)) - 1;
 }
 
-void fill_point_arrays(int nPoints, int vectorLength, int noiseScale,
+void fill_point_arrays(int nPoints, int vectorLength, double noiseScale,
                        double* points1, double* points2);
 
 void fill_hyperplanes(int nPlanes, int vectorLength, double* hyperplanes);
 
 int main() {
-    const int nPoints = 1000;
+    const int nPoints = 1000;     // number of points in the first dataset
+    const int nPoints2 = nPoints; // number of points in the second dataset
     const int vectorLength = 128;
-    const float noiseScale = 0.1;
+    const double noiseScale = 0.1;
 
     // points to be matched
     // LATER: because of their massive size arrays should be dynamically
     //        allocated using 'malloc'
     double points1[nPoints * vectorLength];
-    double points2[nPoints * vectorLength];
+    double points2[nPoints2 * vectorLength];
 
     const int nPlanes = (int)log2(nPoints);
 
@@ -41,9 +42,8 @@ int main() {
     int groupIndexMap[nBoxes];   // The starting index for each group in groupArray
     int groupIndexMapTails[nBoxes]; // temporary values
 
-    // LATER: changed this to contain actual points instead of indices,
-    //        that should be better for performance
     int groupArray[nPoints]; // Actual group
+    int lshMatches[nPoints2]; // holds the actual matches
 
     // - Calculate hash values, keep track of sizes of each group -
     for (int i = 0; i < nPoints; i++) {
@@ -90,9 +90,75 @@ int main() {
         // add the point to the group
         groupArray[idx] = i;
     }
+
+    // - Match points -
+
+    // first calculate hash values (overwriting values in indexGroupMap)
+    for (int i = 0; i < nPoints2; i++) {
+        double* point = &points2[i * vectorLength];
+        int hashcode = 0;  //  hashcode will be the group index
+        // calculate hash value of the i'th point, store resut in indexGroupMap
+        double* hplane = hyperplanes;  // first hyperplane
+        for (int j = 0; j < nPlanes; j++) {
+            // calculate point * hplane
+            double vecMul = 0;
+            for (int k = 0; k < vectorLength; k++) {
+                vecMul += point[k] * hplane[k];
+            }
+            // set i'th bit to one if point is on "positive" side of hyperplane
+            if (vecMul > 0) {
+                hashcode = hashcode | (1 << j);
+            }
+            // next hyperplane
+            hplane += vectorLength;
+        }
+        indexGroupMap[i] = hashcode;  // save the hashcode
+    }
+
+    // then search
+    for (int i = 0; i < nPoints2; i++) {
+        // Find the group of elements from groupArray to match with
+        double bestFitDist = 1e10;
+        int match = -1;
+        int hashcode = indexGroupMap[i];
+        int size = groupSizeMap[hashcode];
+        int startIdx = groupIndexMap[hashcode];
+
+        // Match the points
+        for (int j = startIdx; j < startIdx + size; j++) {
+            int idx = groupArray[j];
+            // diff = sum((points2[i][:] - points1[idx][:]) .^ 2);
+            double diff = 0;
+            for (int k = 0; k < vectorLength; k++) {
+                double tmp = points2[i*vectorLength + k] - points1[idx*vectorLength + k];
+                diff += tmp*tmp;
+            }
+            // check if distance is the lowest distance so far
+            if (diff < bestFitDist) {
+                // printf("bestFitDist=%.3f   diff=%.3f   match=%d", bestFitDist, diff, match);
+                bestFitDist = diff;
+                match = idx;
+            }
+        }
+
+        lshMatches[i] = match;
+    }
+
+
+    // Check how many matches were correct
+    int correct = 0;
+    for (int i = 0; i < nPoints2; i++) {
+        correct += lshMatches[i] == i;
+    }
+    double  correctRatio = ((double) correct) / nPoints2;
+    printf("Correct ratio: %f\n", correctRatio);
+
+    // for (int i = 0; i < nPoints2; i++) {
+    //     printf("%d\n", lshMatches[i]);
+    // }
 }
 
-void fill_point_arrays(int nPoints, int vectorLength, int noiseScale,
+void fill_point_arrays(int nPoints, int vectorLength, double noiseScale,
                        double* points1, double* points2)
 {
     for (int i = 0; i < nPoints; i++) {
