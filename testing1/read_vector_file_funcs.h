b69237f4a241a73260f1e21c9d3397ee415c62ba
Hilmar <hilmarwiderberg@live.no>
2022-01-04 16:49:29 +0100

    Preparing to read files for testing

---
+++ testing1/read_vector_file_funcs.h
@@ -0,0 +1,8 @@
+#ifndef READ_VECTOR_FILE_FUNCS_H
+#define READ_VECTOR_FILE_FUNCS_H
+
+int read_vector_file(char* fPath, void** arr);
+
+int read_vector_file2(char* fPath, void** arr);
+
+#endif /* READ_VECTOR_FILE_FUNCS_H */
