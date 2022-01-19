45d413c98c7519b1061b39bea6659a200ad19534
Hilmar <hilmarwiderberg@live.no>
2022-01-19 22:55:33 +0100

    tried to create some good hyperplanes

---
+++ MLSH_guarantee_matlab/creating_hyperplanes.m
@@ -0,0 +1,166 @@
+% set seed
+rng(0)
+
+%% Create orthogonal hyperplanes
+
+vectorLength = 128;
+nPlanes = 128;
+hyperplanes = zeros(nPlanes, vectorLength);
+
+for i = 1:7
+    val = 1;
+    steps = 2^i;
+    stepLen = vectorLength / steps;
+    for j = 1:stepLen:vectorLength
+        for k = j:(j+stepLen-1)
+            hyperplanes(i,k) = val;
+        end
+        val = -val;
+    end
+end
+
+for i = 1:6
+    for j = 0:(vectorLength-1)
+        hyperplanes(i+7, j+1) = ...
+        hyperplanes(i, mod(j+(2^(6-i)), vectorLength) + 1);
+    end
+end
+
+% % Are they orthogonal? Yes
+% for i = 1:nPlanes
+%     for j = (i+1):nPlanes
+%         fprintf("hyperplanes(%d,:)*hyperplanes(%d,:)' = %f\n",...
+%             i, j, hyperplanes(i,:)*hyperplanes(j,:)');
+%     end
+% end
+
+%% Normalize
+
+for i = 1:13
+    h = hyperplanes(i,:);
+    hyperplanes(i,:) = h / sqrt(h*h');
+end
+
+% nNewPlanes = nPlanes - 13;
+% newPlanes = rand(nNewPlanes, vectorLength);
+
+%%  Calculate random hyperplanes that are also normal to the previous ones
+
+for i = 14:nPlanes
+    new = rand(1, vectorLength);
+    for j = 1:(i-1)
+        new = new - (new * hyperplanes(j,:)') * hyperplanes(j,:);
+    end
+    hyperplanes(i,:) = new / sqrt(new*new');
+end
+
+% % Are they orthogonal? Yes
+% for i = 1:nPlanes
+%     for j = (i+1):nPlanes
+%         fprintf("hyperplanes(%d,:)*hyperplanes(%d,:)' = %f\n",...
+%             i, j, hyperplanes(i,:)*hyperplanes(j,:)');
+%     end
+% end
+% 
+% % What do the elements sum to?
+% for i = 1:nPlanes
+%     fprintf("sum(hyperplanes(%d,:)) = %f\n", i, sum(hyperplanes(i,:)));
+% end
+
+%% Find the ones with equal sum distribution
+
+nNewPlanes = nPlanes - 13;
+inner_sum = zeros(nNewPlanes,1);
+abs_inner_sum = zeros(nNewPlanes,1);
+
+for i = 1:nNewPlanes
+    inner_sum(i) = sum(hyperplanes(i+13,:));
+    abs_inner_sum(i) = abs(inner_sum(i));
+end
+
+[abs_inner_sum2, index] = sort(abs_inner_sum);
+
+inner_sum2 = zeros(nNewPlanes,1);
+new_hyperplanes = zeros(nNewPlanes,vectorLength);
+
+for i = 1:nNewPlanes
+    new_hyperplanes(i,:) = hyperplanes(index(i)+13,:);
+    inner_sum2(i) = inner_sum(index(i));
+end
+
+% % Correct sorting? Yes
+% fprintf("%f\n", max(abs(sum(new_hyperplanes(i,:)) - inner_sum2(i))));
+
+%% Make the hyperplanes sum to 0 while still being orthogonal
+%  to the original hyperplanes
+
+for i = 1:57
+    new_hyperplanes(i,:) = new_hyperplanes(i,:) ...
+        - ((inner_sum2(i) / inner_sum2(57+i))...
+          * new_hyperplanes(57+i,:));
+end
+
+inner_sum3 = zeros(nNewPlanes,1);
+for i = 1:nNewPlanes
+    inner_sum3(i) = sum(new_hyperplanes(i,:));
+end
+
+new_hyperplanes2 = new_hyperplanes(1:57,:);
+
+% Normalize again
+for i = 1:57
+    h = new_hyperplanes2(i,:);
+    new_hyperplanes2(i,:) = h / sqrt(h*h');
+end
+
+positiveRatio = zeros(57,1);
+for i = 1:57
+    positiveRatio(i) = sum(new_hyperplanes2(i,:) > 0) / 128;
+end
+positiveRatio = positiveRatio - mean(positiveRatio);
+positiveRatio = positiveRatio / std(positiveRatio);
+positiveRatio = abs(positiveRatio);
+
+meanMedianDifference = zeros(57,1);
+for i = 1:57
+    meanMedianDifference(i) = ...
+        abs(mean(new_hyperplanes2(i,:)) - median(new_hyperplanes2(i,:)));
+end
+meanMedianDifference = meanMedianDifference - mean(meanMedianDifference);
+meanMedianDifference = meanMedianDifference / std(meanMedianDifference);
+meanMedianDifference = abs(meanMedianDifference);
+
+% Create some cost function that I want to sort by, I only really need some
+% of them. Just want the distribution to be compact if possible, not really
+% too important.
+cost = positiveRatio + meanMedianDifference;
+
+[r, index] = sort(cost);
+
+for i = 1:57
+    new_hyperplanes2(i,:) = new_hyperplanes2(index(i),:);
+end
+
+%% Add them to the final hyperplanes
+
+final_hplanes = zeros(70,128);
+final_hplanes(1:13,:) = hyperplanes(1:13,:);
+final_hplanes(14:70,:) = new_hyperplanes2(:,:);
+
+% Are they orthogonal? Yes
+for i = 1:70
+    for j = (i+1):70
+        fprintf("final_hplanes(%d,:)*final_hplanes(%d,:)' = %f\n",...
+            i, j, final_hplanes(i,:)*final_hplanes(j,:)');
+    end
+end
+
+% What do the elements sum to?
+for i = 1:70
+    fprintf("sum(final_hplanes(%d,:)) = %f\n", i, sum(final_hplanes(i,:)));
+end
+
+
+fileID = fopen('hyperplanes.dat','w');
+fwrite(fileID,final_hyperplanes,'float32');
+fclose(fileID);
