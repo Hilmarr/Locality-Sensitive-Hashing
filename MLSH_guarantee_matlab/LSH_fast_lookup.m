900122f636ec6fb6246c456900f88062320977e4
Hilmar <hilmarwiderberg@live.no>
2022-01-19 14:44:11 +0100

    Created version where the base points are replicated on the other side of hyperplanes if they are close to the hyperplanes instead of the query vectors searching the other side of several hyperplanes

--- MLSH_guarantee_matlab/LSH_fast_lookup.m
+++ MLSH_guarantee_matlab/LSH_fast_lookup.m
@@ -77,6 +77,13 @@ lshStart = tic;
 %         * (maxHPLength * rand(1) / norm(hyperplanes(i,:), 2));
 % end
 
+% Squared dists from hyperplanes
+sqrdDists = zeros(nPoints, nPlanes);
+
+% Should later be naturally extended like an arraylist
+groupMapExt = zeros(nPoints*5,1);
+groupMapExtIndices = zeros(nPoints + 1, 1);
+
 % Initialize LSH tables
 indexGroupMap = zeros(nPoints, 1);
 nBoxes = 2^nPlanes;
@@ -84,65 +91,12 @@ groupSizeMap = zeros(nBoxes, 1);
 groupIndexMap = zeros(nBoxes, 1);
 groupIndexMapTails = zeros(nBoxes, 1);
 
-% array of points grouped by what box they got hashed into
-groupArray = zeros(nPoints, 1);
-
 % - Put points1 into our lsh table -
 
 % Calculate hash values, keep track of sizes of each group
 hashValuesStart1 = tic;
 for i = 1:nPoints
     point = points1(i,:);
-    hashcode = 0;
-    for j = 1:nPlanes
-        hplane = hyperplanes(j,:)';
-        if (point*hplane > 0)
-            hashcode = bitor(hashcode, bitshift(1, j-1));
-        end
-    end
-    hashcode = hashcode+1; % Because matlab is weird with indexing
-    indexGroupMap(i) = hashcode;
-    groupSizeMap(hashcode) = groupSizeMap(hashcode)+1;
-end
-hashValuesTime1 = toc(hashValuesStart1);
-
-% Prepare the index map
-cnt = 1;
-for i = 1:nBoxes
-    groupIndexMap(i) = cnt;
-    groupIndexMapTails(i) = groupIndexMap(i);
-    cnt = cnt + groupSizeMap(i);
-end
-
-for i = 1:nPoints
-    % What group did the point get hashed into
-    hashcode = indexGroupMap(i);
-    % What is the start index of that group in groupArray
-    idx = groupIndexMapTails(hashcode);
-    
-    % Increment the index tail mapping
-    groupIndexMapTails(hashcode) = idx + 1;
-    
-    groupArray(idx) = i;
-    
-end
-
-tablesCreatedTime = toc(lshStart);
-
-
-%% Match points2 with points1 using LSH hash table
-matchingStart = tic;
-
-% Squared dists from hyperplanes
-sqrdDists = zeros(nPoints, nPlanes);
-
-% Calculating hash codes happens separately from searching,
-% this makes the program easier to parallelize at some later point
-
-% Calculate hash values
-hashValuesStart2 = tic;
-for i = 1:nPoints
-    point = points2(i,:);
     hashcode = 0;
     for j = 1:nPlanes
         hplane = hyperplanes(j,:)';
@@ -154,40 +108,9 @@ for i = 1:nPoints
     end
     hashcode = hashcode+1; % Because matlab is weird with indexing
     indexGroupMap(i) = hashcode;
+%     groupSizeMap(hashcode) = groupSizeMap(hashcode)+1;
 end
-hashValuesTime2 = toc(hashValuesStart2);
-
-% for i = 1:nPoints
-%     fprintf("Point %d:\n", i);
-%     for j = 1:nPlanes
-%         fprintf("  dist from h%d = %f\n", j, sqrt(sqrdDists(i,j)));
-%     end
-%     fprintf("\n");
-% end
-
-% Should later be naturally extended like an arraylist
-groupMapExt = zeros(nPoints*5,1);
-groupMapExtIndices = zeros(nPoints + 1, 1);
-
-% cnt = 1;
-% for i = 1:nPoints
-%     hashcode = indexGroupMap(i) - 1;
-% %     fprintf("hashcode%d = %d\n", i, hashcode);
-%     groupMapExtIndices(i) = cnt;
-%     groupMapExt(cnt) = hashcode + 1;
-%     cnt = cnt + 1;
-%     % Replace this with a recursive function that can also find
-%     % combination of these values
-%     for j = 1:nPlanes
-%         if (sqrdDists(i,j) < threshold)
-%             groupMapExt(cnt) = bitxor(hashcode, bitshift(1, j-1)) + 1;
-% %             fprintf("  j=%d, hashcode=%d\n", j, groupMapExt(cnt));
-%             cnt = cnt + 1;
-%         end
-%     end
-% %     fprintf("\n");
-% end
-% groupMapExtIndices(nPoints + 1) = cnt;
+hashValuesTime1 = toc(hashValuesStart1);
 
 cnt = 1;
 for i = 1:nPoints
@@ -198,53 +121,82 @@ for i = 1:nPoints
         findCloseNeighbors(sqrdDists(i,:),threshold,nPlanes,hashcode);
     for j = 1:hc_num
         groupMapExt(cnt) = hc_neighbors(j) + 1;
+        groupSizeMap(hc_neighbors(j)+1) = groupSizeMap(hc_neighbors(j)+1) + 1;
         cnt = cnt + 1;
     end
 end
 groupMapExtIndices(nPoints + 1) = cnt;
 
-% for i = 1:nPoints
-%     fprintf("Point %d hash codes:\n", i);
-% %     for j = groupMapExtIndices(i):(groupMapExtIndices(i+1)-1)
-% %         fprintf("  %s\n", dec2bin(groupMapExt(j), nPlanes));
-% %     end
-%     for j = groupMapExtIndices(i):(groupMapExtIndices(i+1)-1)
-%         fprintf("  %d\n", groupMapExt(j));
-%     end
-%     fprintf("\n");
-% end
-% 
+% array of points grouped by what box they got hashed into
+groupArray = zeros(cnt-1, 1);
 
+% Prepare the index map
+cnt = 1;
+for i = 1:nBoxes
+    groupIndexMap(i) = cnt;
+    groupIndexMapTails(i) = groupIndexMap(i);
+    cnt = cnt + groupSizeMap(i);
+end
+
+for i = 1:nPoints
+    for j = groupMapExtIndices(i):(groupMapExtIndices(i+1)-1)
+        % What group did the point get hashed into
+        hashcode = groupMapExt(j);
+        % What is the start index of that group in groupArray
+        idx = groupIndexMapTails(hashcode);
+        % Increment the index tail mapping
+        groupIndexMapTails(hashcode) = idx + 1;
+        groupArray(idx) = i;
+    end
+end
+
+tablesCreatedTime = toc(lshStart);
+
+
+%% Match points2 with points1 using LSH hash table
+matchingStart = tic;
+
+% Calculating hash codes happens separately from searching,
+% this makes the program easier to parallelize at some later point
+
+% Calculate hash values
+hashValuesStart2 = tic;
+for i = 1:nPoints
+    point = points2(i,:);
+    hashcode = 0;
+    for j = 1:nPlanes
+        hplane = hyperplanes(j,:)';
+        if (point*hplane > 0)
+            hashcode = bitor(hashcode, bitshift(1, j-1));
+        end
+    end
+    hashcode = hashcode+1; % Because matlab is weird with indexing
+    indexGroupMap(i) = hashcode;
+end
+hashValuesTime2 = toc(hashValuesStart2);
 
 % Match the points using lsh
 matching_lsh = zeros(nPoints, 1);
 
 for i = 1:nPoints
-    
+    % Find table and elements in points1 to match with
     shortestDist = 1e10;
+    hashcode = indexGroupMap(i);
+    size = groupSizeMap(hashcode);
+    startIdx = groupIndexMap(hashcode);
     
-    for k = groupMapExtIndices(i):(groupMapExtIndices(i+1)-1)
-        
-        % Find table and elements in points1 to match with
-        hashcode = groupMapExt(k);
-        size = groupSizeMap(hashcode);
-        startIdx = groupIndexMap(hashcode);
-
-        % Match the points
-        for j = startIdx:(startIdx+size-1)
-            % Take euclidean distance between the vector
-            idx = groupArray(j);
-            diff = sum((points2(i,:) - points1(idx,:)) .^ 2);
-            % If euclidean distance is better than the best match,
-            % we have a new best match
-            if (diff < shortestDist)
-                shortestDist = diff;
-                match = idx;
-%                 fprintf("i=%d, match=%d\n",i,idx);
-            end
+    % Match the points
+    for j = startIdx:(startIdx+size-1)
+        % Take euclidean distance between the vector
+        idx = groupArray(j);
+        diff = sum((points2(i,:) - points1(idx,:)) .^ 2);
+        % If euclidean distance is better than the best match,
+        % we have a new best match
+        if (diff < shortestDist)
+            shortestDist = diff;
+            match = idx;
         end
     end
-    
     if (shortestDist ~= 1e10)
         matching_lsh(i) =  match;
     end
@@ -253,6 +205,7 @@ end
 matchingTime = toc(matchingStart);
 lshTime = toc(lshStart);
 hashValuesTime = hashValuesTime1 + hashValuesTime2;
+
 
 
 
@@ -287,7 +240,6 @@ fprintf("  time portion: hyperplane,vector multiplication:  %f%% \n", ...
 disp(" *");
 fprintf("  time portion spent creating and filling tables:  %f%% \n", ...
     100*tablesCreatedTime/lshTime);
-disp(" *");
 fprintf("  time portion spent matching 2nd dataset:         %f%% \n", ...
     100*matchingTime/lshTime);
 fprintf("\n\n");
