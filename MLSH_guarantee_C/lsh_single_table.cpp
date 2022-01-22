308347b2189af1a21879b9e11ce6b7e1d254b99c
Hilmar <hilmarwiderberg@live.no>
2022-01-22 14:33:13 +0100

    calculated hash values and organized points into groups

--- MLSH_guarantee_C/lsh_single_table.cpp
+++ MLSH_guarantee_C/lsh_single_table.cpp
@@ -12,14 +12,89 @@
 
 #include "read_vector_file_funcs.h"
 
+// Globally defined so that the compiler might make assumptions about it
+// later on if expedient
 const int vectorLength = 128;
-const int nPlanes = 30;
+
+void calculate_hash_values(
+    // input
+    int nPoints, float* __restrict__ points,
+    int nPlanes, float* __restrict__ hyperplanes,
+    // output
+    int* __restrict__ indexGroupMap)
+{
+    // - Calculate hash values, keep track of sizes of each group -
+    for (int i = 0; i < nPoints; i++) {
+
+        float* point = &points[i * vectorLength];
+        int hashcode = 0;  //  hashcode will be the group index
+
+        // calculate hash value of the i'th point, store resut in indexGroupMap
+        for (int j = 0; j < nPlanes; j++) {
+            float* hplane = &hyperplanes[j * vectorLength];  // first hyperplane
+            // calculate point * hplane
+            float vecMul = 0;
+            for (int k = 0; k < vectorLength; k++) {
+                vecMul += point[k] * hplane[k];
+            }
+            // set i'th bit to one if point is on "positive" side of hyperplane
+            if (vecMul > 0) {
+                hashcode = hashcode | (1 << j);
+            }
+        }
+        indexGroupMap[i] = hashcode;  // save the hashcode
+    }
+}
+
+void organize_points_into_groups(
+    // input
+    int nPoints, int nGroups,
+    int* __restrict__ indexGroupMap,
+    // output
+    int* __restrict__ groupArray,
+    int* __restrict__ groupIndexMap)
+{
+    int* groupSizeMap = (int*) calloc(nGroups, sizeof(int));
+    int* groupIndexMapTails = (int*) calloc(nGroups, sizeof(int));
+
+    // find the size of each group
+    for (int i = 0; i < nPoints; i++) {
+        groupSizeMap[indexGroupMap[i]]++;
+    }
+
+    // Find group indices into the group array 'groupArray'
+    // // find group indices using exclusive scan of the group sizes
+    //  std::exclusive_scan(groupSizeMap, groupSizeMap + nGroups - 1);
+    // doing it manually for now
+    int cnt = 0;
+    for (int i = 0; i < nGroups; i++) {
+        groupIndexMap[i] = cnt;
+        groupIndexMapTails[i] = cnt;
+        cnt += groupSizeMap[i];
+        // printf("cnt = %d\n", cnt);
+    }
+    groupIndexMap[nGroups] = cnt;
+
+    // Fill groupArray
+    for (int i = 0; i < nPoints; i++) {
+        // what group did the point get hashed into
+        int hashcode = indexGroupMap[i];
+        // start index of that group in groupArray + number of elements currently inserted
+        int idx = groupIndexMapTails[hashcode]++;
+        // add the point to the group
+        groupArray[idx] = i;
+    }
+    
+    free(groupSizeMap);
+    free(groupIndexMapTails);
+}
 
 int main(int argc, char** argv)
 {
     int nBaseVecs = 0;    // number of base vectors
     int nQueryVecs = 0;   // number of query vectors
 
+    const int nPlanes = 8;
 
     // -----    Read data    -----
 
@@ -59,6 +134,24 @@ int main(int argc, char** argv)
     float* baseVecs = (float*)tmp1;
     float* queryVecs = (float*)tmp2;
 
+    // TODO: make LSH tables
+
+    //  -- Arrays to organize groups --
+    const int nGroups = 1 << nPlanes;
+    // the group that each point falls into
+    int* indexGroupMap = (int*)malloc(nBaseVecs * sizeof(int));
+    // // distance from each hyperplane for each point
+    // float* distanceFromHP = (float*) malloc(nBaseVecs * nPlanes * sizeof(float));
+
+    int* groupIndexMap = (int*) malloc((nGroups + 1) * sizeof(int));
+    int* groupArray = (int*) malloc(nBaseVecs * sizeof(int));
+
+    calculate_hash_values(nBaseVecs, baseVecs, nPlanes, hyperplanes,
+                          indexGroupMap);
+
+    organize_points_into_groups(nBaseVecs, nGroups, indexGroupMap,
+                                groupArray, groupIndexMap);
+
     // TODO: match
 
     // - Read ground truth -
@@ -74,6 +167,12 @@ int main(int argc, char** argv)
 
 
     // free memory
+    
+    free(indexGroupMap);
+    // free(distanceFromHP);
+    free(groupIndexMap);
+    free(groupArray);
+
     free(hyperplanes);
     free(baseVecs);
     free(queryVecs);
