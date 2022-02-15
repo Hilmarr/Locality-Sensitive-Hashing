8c0364a2a463ac590d864976b23519f3c5db0c17
Hilmar <hilmarwiderberg@live.no>
2022-02-15 13:10:39 +0100

    Added some new hyperplanes that are almost zero sum

--- creating_hyperplanes/creating_random_hplanes.m
+++ creating_hyperplanes/creating_random_hplanes.m
@@ -5,23 +5,45 @@ vectorLength = 128;
 
 hPlanes = 2*rand(2*nPlanes, vectorLength) - 1;
 
+% for i = 1:nPlanes
+%     sum1 = sum(hPlanes(i,:));
+%     sqrdSize = hPlanes(i,:)*hPlanes(i,:)';
+%     fprintf("%f, %f\n", sum1, sqrdSize);
+% end
+
+% Make them sum to zero
 for i = 1:nPlanes
     sum1 = sum(hPlanes(i,:));
     sum2 = sum(hPlanes(i+nPlanes,:));
     hPlanes(i,:) = hPlanes(i,:) - (sum1/sum2) * hPlanes(i+nPlanes,:);
 end
 
+% Normalize vector
 for i = 1:nPlanes
     hPlanes(i,:) = hPlanes(i,:) / sqrt(hPlanes(i,:)*hPlanes(i,:)');
 end
 
 hPlanes = hPlanes(1:1000,:);
 
+% % Create normalized noise
+% noise = 2*rand(nPlanes, vectorLength) - 1;
 % for i = 1:nPlanes
-%     sum1 = sum(hPlanes(i,:));
-%     sqrdSize = hPlanes(i,:)*hPlanes(i,:)';
-%     fprintf("%f, %f\n", sum1, sqrdSize);
+%     noise(i,:) = noise(i,:) / sqrt(noise(i,:)*noise(i,:)');
 % end
+% 
+% % Add noise and normalize
+% for i = 1:nPlanes
+%     hPlanes(i,:) = hPlanes(i,:) + 0.3 * noise(i,:);
+%     hPlanes(i,:) = hPlanes(i,:) / sqrt(hPlanes(i,:)*hPlanes(i,:)');
+% end
+
+for i = 1:nPlanes
+    sum1 = sum(hPlanes(i,:));
+    sqrdSize = hPlanes(i,:)*hPlanes(i,:)';
+    fprintf("%f, %f\n", sum1, sqrdSize);
+end
+
+
 
 fileID = fopen('hyperplanes.dat','w');
 fwrite(fileID,hPlanes','float32');
